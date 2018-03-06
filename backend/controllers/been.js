e69db128f35b9e1f78a8464dc2301852c5282117
hyojin.sarchet <hyojin5931@hanmail.net>
2018-03-05 22:03:45 -0500

    added CRUD

--- backend/controllers/been.js
+++ backend/controllers/been.js
@@ -8,36 +8,53 @@ const Been = mongoose.model('Been')
 
 // define the routes
 router.get('/', (req, res) => {
-    Been.find({})
-        .then(beens => res.json(beens))
-})
-
-
-// get a country by its id: '/:id'
-// grab name using mongoose
-router.get('/:name', (req, res) => {
-    Been.findOne({name: req.params.name})
-        .then(beens => res.json(beens))
+    Been.find({}).then(beens => {
+        res.render('beens/index', {beens})
+    })
 })
 
 
 // create new countries
 router.post('/', (req, res) => {
-    Been.create(req.body)
-        .then(beens => res.json(beens))
+    Been.create({
+        name: req.body.name,
+        lat: req.body.lat,
+        lng: req.body.lng
+    }).then(() => {
+      res.redirect('/beens')
+    })
 })
 
+router.get("/new", (req, res) => {
+    res.render("beens/new")
+})
+
+router.get('/edit/:id', (req, res) => {
+    Been.findOne({_id: req.params.id}).then(been => {
+        res.render('beens/edit', been)
+    })
+})
 
 // update a country by id
-router.put('/:name', (req, res) => {
-    Been.findOneAndUpdate({name: req.params.name}, req.body)
-        .then(beens => res.json(beens))
+router.put('/:id', (req, res) => {
+    Been.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}).then(
+      been => {
+        res.redirect('/beens')
+      }
+    )
+})
+
+router.get("/:id", (req, res) => {
+  Been.findOne({ _id: req.params.id }).then(been => {
+    res.render("beens/show", been)
+  })
 })
 
 // delete a country by id
-router.delete('/:name', (req, res) => {
-    Been.findOneAndRemove({name: req.params.name})
-        .then(beens => res.json(beens))
+router.delete('/:id', (req, res) => {
+    Been.findOneAndRemove({_id: req.params.id}).then(() => {
+        res.redirect('/beens')
+    })
 })
 
 // export the router
